96a22577335c6c5656f82f1a66b594debd6eb1d6
eajr <me@ealfonso.com>
2019-01-03 22:15:30 -0500

    EchoService

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EchoService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : ControllerBase
+    {
+        // GET api/echo/hello
+        [HttpGet("{message}")]
+        public ActionResult<string> Get(string message)
+        {
+            return message;
+        }
+    }
+}
